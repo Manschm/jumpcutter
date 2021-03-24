a48490ea4e3e4bfd361e096913a417327431141b
Manuel Schmid <manuel_schmid@hotmail.com>
2021-03-24 14:32:41 +0100

    Removed unused frame_rate argument

--- jumpcutter.py
+++ jumpcutter.py
@@ -61,14 +61,11 @@ parser.add_argument('--silent_speed', type=float, default=5.00,
 parser.add_argument('--frame_margin', type=float, default=1,
                     help="some silent frames adjacent to sounded frames are included to provide context. How many frames on either the side of speech should be included? That's this variable.")
 parser.add_argument('--sample_rate', type=float, default=44100, help="sample rate of the input and output videos")
-parser.add_argument('--frame_rate', type=float, default=30,
-                    help="frame rate of the input and output videos. optional... I try to find it out myself, but it doesn't always work.")
 parser.add_argument('--frame_quality', type=int, default=3,
                     help="quality of frames to be extracted from input video. 1 is highest, 31 is lowest, 3 is the default.")
 
 args = parser.parse_args()
 
-frameRate = args.frame_rate
 SAMPLE_RATE = args.sample_rate
 SILENT_THRESHOLD = args.silent_threshold
 FRAME_SPREADAGE = args.frame_margin
